8ea4c4f9d5f91a17d0e6ad1f9ca07d2d2af116b4
Shang <shang1219178163@gmail.com>
2019-12-13 09:01:03 +0800

    update

---
+++ templates/ios/Example/PROJECT/CPDHomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  CPDHomeViewController.h
+//  PROJECT_Example
+//
+//  Created by Bin Shang on 2019/12/13.
+//  Copyright © 2019 PROJECT_OWNER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CPDHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ templates/ios/Example/PROJECT/CPDHomeViewController.m
@@ -0,0 +1,28 @@
+//
+//  CPDHomeViewController.m
+//  PROJECT_Example
+//
+//  Created by Bin Shang on 2019/12/13.
+//  Copyright © 2019 PROJECT_OWNER. All rights reserved.
+//
+
+#import "CPDHomeViewController.h"
+
+@interface CPDHomeViewController ()
+
+@end
+
+@implementation CPDHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"Home";
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+}
+
+@end
