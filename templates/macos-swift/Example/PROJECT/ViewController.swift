04b2827207a49ea3c1bf2eb704647b83b20f7050
Shang <shang1219178163@gmail.com>
2020-03-23 12:53:18 +0800

    完善

--- templates/macos-swift/Example/PROJECT/ViewController.swift
+++ templates/macos-swift/Example/PROJECT/ViewController.swift
@@ -9,18 +9,26 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    override func loadView() {
+        // 设置 ViewController 大小同 mainWindow
+        guard let windowRect = NSApplication.shared.mainWindow?.frame else { return }
+        view = NSView(frame: windowRect)
+    }
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
     // Do any additional setup after loading the view.
-  }
-
-  override var representedObject: Any? {
-    didSet {
-    // Update the view, if already loaded.
+        view.wantsLayer = true;
+        view.layer!.backgroundColor = NSColor.white.cgColor;
     }
-  }
+
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
 
 
 }
